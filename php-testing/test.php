fc9a13061efc7e1fad1a47959242a70f68f19454
Gavin Wainwright <Minerdude010@gmail.com>
2017-03-04 08:36:20 -0500

    Create test.php

---
+++ php-testing/test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+  <?php
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
+    
+  ?>
+  </body>
+</html>
