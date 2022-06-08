29cd655c84fe1784fef5bff9a8201a6cd99b713a
Gavin Wainwright <gav.wainwright@gmail.com>
2022-06-07 20:29:49 -0400

    tab

--- public/js/main.js
+++ public/js/main.js
@@ -78,9 +78,9 @@ function updateProgressBar() {
 
 function setCookie(name, value) {
   var cookie = [
-      name,
-      "",
-      JSON.stringify(value)
+    name,
+    "",
+    JSON.stringify(value)
   ].join('');
   document.cookie = cookie;
 }
@@ -89,13 +89,13 @@ function readCookie(name) {
   var nameEQ = name + "=";
   var ca = document.cookie.split(";");
   for(var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) === 0) {
-          return JSON.parse(
-              c.substring(nameEQ.length, c.length)
-          );
-      }
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) {
+      return JSON.parse(
+        c.substring(nameEQ.length, c.length)
+      );
+    }
   }
   return null;
 }
